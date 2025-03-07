e673def81375c67b2bb412f7402b7df3c43d6340
Arthur Bore <arthur.bore@icloud.com>
2025-03-07 10:20:52 +0100

    Finalisation test association + commentaire

--- tests/associations.test.js
+++ tests/associations.test.js
@@ -1,19 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
+
 const mongoose = require("mongoose");
 require("dotenv").config();
-const Association = require("../models/associations");
-
-// TEST INITIAL DE SUPERTEST
-describe("Test the root path", () => {
-  test("It should respond to the initial GET method", async () => {
-    const res = await request(app).get("/");
-
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-// TEST RÉCUPÉRATION D'ASSOCIATIONS ALÉATOIRES
+// --- TEST 1 --- //
+// RÉCUPÉRATION DE 50 ASSOCIATIONS AU HASARD
 describe("GET /associations/randomall", () => {
   beforeAll(() => { // AVANT TOUT, ON EXECUTE CECI :
     return mongoose // CONNECTION À LA BDD
@@ -21,18 +12,17 @@ describe("GET /associations/randomall", () => {
   });
 
   afterAll(() => { // APRÈS TOUT, ON EXECUTE CECI :
-    return mongoose.connection.close()
+    return mongoose.connection.close() // DECONNECTION À LA BDD
   });
 
   it("Should return up to 50 random associations", () => { // TEST
     return request(app)
-      .get("/associations/randomall")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body.result).toBe(true);
-        expect(res.body.data.length).toBeLessThanOrEqual(50);
+      .get("/associations/randomall") // DIRECTION VERS LA ROUTE
+      .expect("Content-Type", /json/) // EN JSON
+      .then((res) => { // ON VÉRIFIE SI LA ROUTE NOUS RENVOIE BIEN
+        expect(res.statusCode).toBe(200); // UN STATUSCODE 200
+        expect(res.body.result).toBe(true); // UN RESULT = À TRUE
+        expect(res.body.data.length).toBeLessThanOrEqual(50); // UN TABLEAU DE DONNÉES <= 50
       });
   });
 });
