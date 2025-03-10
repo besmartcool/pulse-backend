4b0e8c3ff1718deed959a2fa94abbc6cb30b2b1b
Patrice <monat.patrice@gmail.com>
2025-03-10 12:55:25 +0100

    pulse route users

--- routes/users.js
+++ routes/users.js
@@ -19,16 +19,9 @@ router.post("/signup", (req, res) => {
   User.findOne({ email: req.body.email }).then((data) => {
     if (data === null) {
       const newUser = new User({
-        // username: req.body.username,
-        // firstname: req.body.firstname,
-        // lastname: req.body.lastname,
         email: req.body.email,
         password: hash,
         token: uid2(32),
-        // residenceCountry: req.body.residenceCountry,
-        // nationality: req.body.nationality,
-        // destinationCountry: req.body.destinationCountry,
-        // profilePicture: req.body.profilePicture,
       });
 
       newUser.save().then((user) => {
@@ -56,37 +49,22 @@ router.post("/signup", (req, res) => {
     });
   });
 
-  router.post("/signup", (req, res) => {
-    if (!checkBody(req.body, ["email", "password"])) {
-      res.json({ result: false, error: "Missing or empty fields" });
-      return;
-    };
-    const hash = bcrypt.hashSync(req.body.password, 10);
-  
-    
-    User.findOne({ email: req.body.email }).then((data) => {
-      if (data === null) {
-        const newUser = new User({
-          // username: req.body.username,
-          // firstname: req.body.firstname,
-          // lastname: req.body.lastname,
-          email: req.body.email,
-          password: hash,
-          token: uid2(32),
-          // residenceCountry: req.body.residenceCountry,
-          // nationality: req.body.nationality,
-          // destinationCountry: req.body.destinationCountry,
-          // profilePicture: req.body.profilePicture,
+  router.post("/", (req, res) => {
+   
+    User.updateMany({ token: req.body.token }, {
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      residenceCountry: req.body.residenceCountry,
+      nationality: req.body.nationality,
+      destinationCountry: req.body.destinationCountry,
+      // profilePicture: req.body.profilePicture,
+    }).then((data) => {
+      res.json({ result: true,  });
         });
   
-        newUser.save().then((user) => {
-          res.json({ result: true, token: user.token, email: user.email });
-        });
-      } else {
-        res.json({ result: false, error: "User already exists" });
-      }
-    });
-    });
+      });
+       
 
 
 module.exports = router;
