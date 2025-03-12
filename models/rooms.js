7e45881e894dda526e214ff3521e9ca0befa1865
Arthur Bore <arthur.bore@icloud.com>
2025-03-12 13:00:39 +0100

    Finalisation du chat - routes

--- models/rooms.js
+++ models/rooms.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const roomSchema = new mongoose.Schema({
   name: String, // Nom de la room
   users: [String], // Liste des emails des utilisateurs dans la room
-  createdAt: { type: Date, default: Date.now }
+  lastMessage: String, // Dernier message envoyé
+  lastMessageAt: Date, // Date du dernier message
+  createdAt: { type: Date, default: Date.now } // Date de création
 });
 
 const Room = mongoose.model('Room', roomSchema);
